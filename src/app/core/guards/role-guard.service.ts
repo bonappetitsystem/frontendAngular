ade18730c58d0e0f481e47c511893e0312ab3844
Aciole <joyce.aciole@capgemini.com>
2022-09-09 11:53:23 -0300

    correções de sonar cloud css e html

--- src/app/core/guards/role-guard.service.ts
+++ src/app/core/guards/role-guard.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
